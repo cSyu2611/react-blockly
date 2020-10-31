f3f910a99710a60bfa999887666000fd8e022212
LimeYuzu2611 <56949613+cSyu2611@users.noreply.github.com>
2020-11-01 01:24:52 +0900

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,4 @@
-const localAPI = "//ec2-3-19-244-115.us-east-2.compute.amazonaws.com:8000"
+const localAPI = "http://ec2-3-19-244-115.us-east-2.compute.amazonaws.com:8000"
 
 const headerTitle = "Blockly concordancer"
 
